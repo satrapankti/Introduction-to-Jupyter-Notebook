fe0a26e1d2085f2d1f743147a0f4849e70a8fd16
Pankti Satra <satrapankti26@gmail.com>
2023-03-14 11:10:49 +0530

    Create MyMathUtility.py

---
+++ Practice/Modules/MyMathUtility.py
@@ -0,0 +1,11 @@
+def printRomanValue(num):
+    if num == 1:
+        print("I")
+    elif num == 2:
+        print("II")
+    elif num == 3:
+        print("III")
+    elif num == 4:
+        print("IV")
+    elif num == 5:
+        print("V")
